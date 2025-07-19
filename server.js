b28241de73cea5d35f05e24c07f26ba4b2953374
Bharat8327 <bp609213@gmail.com>
2025-07-20 04:36:01 +0530

    correct port

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use((req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
